ae87961cdb94dcada3d6112a27484cdeda8f8f7f
PatriciaaHarris <patricia.harris.c@gmail.com>
2025-11-03 19:33:28 -0300

    yep

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,3 +1,7 @@
+//List<int> numbers;
+//List<string> words;
+
+
 using System;
 
 class Program
@@ -5,5 +9,83 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers,type 0 when finished");
+        int number= Console.ReadLine()
+        
+        While (number != 0)
+        Console.Write("Enter number :")
+        int idea = Console.ReadLine()
+
+
+
+
+
+
+
+
+    
+
+        
+
+
+        Console.Write("Enter ")
     }
 }
+
+
+
+List<int> numbers = new List<int>();
+//numbers = []
+
+int userNumber = -1;
+while (userNumber != 0)
+{
+    userNumber = int.Parse(Console.ReadLine());
+    if (userNumber != 0)
+        numbers.Add(userNumber);
+}
+//user_number = -1
+while user_number != 0:
+    user_number = int(input("Enter a number: "))
+    if user_number != 0:
+        numbers.append(user_number)
+
+
+int sum = 0;
+foreach (int number in numbers)
+{
+    sum += number;
+}
+//total = sum(numbers)
+
+
+float average = ((float)sum) / numbers.Count;
+//average = total / len(numbers)
+
+
+int max = numbers[0];
+foreach (int number in numbers)
+{
+    if (number > max)
+        max = number;
+}
+//max_number = max(numbers)
+
+
+int minPositive = int.MaxValue;
+foreach (int number in numbers)
+{
+    if (number > 0 && number < minPositive)
+        minPositive = number;
+}
+//min_positive = min(n for n in numbers if n > 0)
+
+numbers.Sort();
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+//numbers.sort()
+for n in numbers:
+    print(n)
+
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,77 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the rpogram!")
+
+        }
+         void DisplayPersonalMessage(string userName)
+         {
+            Console.WriteLine($"Please enter your name :" )
+        
+         }
+
+         void PromptUserNumber(string userNumber)
+         {
+            Console.WriteLine($"Please enter your number :" )
+        
+         }
+
+         staticint AddNumbers (int )
+
+
+
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+}
     }
 }
