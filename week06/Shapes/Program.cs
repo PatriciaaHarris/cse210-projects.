bd6cc218856cdf5d80548f67e7d9c61d2d94a454
Tu Nombre <tuemail@example.com>
2025-12-04 21:12:54 -0300

    yep

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+static void Main(string[] args)
+{
+List<Shape> shapes = new List<Shape>();
+
+
+    shapes.Add(new Square("Red", 5));
+    shapes.Add(new Rectangle("Blue", 4, 3));
+    shapes.Add(new Circle("Green", 2));
+
+    foreach (Shape shape in shapes)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.WriteLine($"Color: {shape.GetColor()}");
+        Console.WriteLine($"Area: {shape.GetArea()}");
+        Console.WriteLine();
     }
+}
+
+
 }
